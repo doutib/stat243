45d3bf7fda664ec72f378f244a8a77ee69978792
doutib <thibault.doutre@gmail.com>
2015-10-25 16:32:50 -0700

    2dbis

--- ps6/ScriptQ2d.R
+++ ps6/ScriptQ2d.R
@@ -7,6 +7,7 @@ drv = dbDriver("SQLite")
 db = dbConnect(drv, dbname = fileName)
 
 # Create index
+print('Creating Index...')
 dbSendQuery(db,"CREATE INDEX index_name ON airline1 (Origin,Dest,Month,DayOfWeek,CRSDepTime,DepDelay)")
 
 # Create required table with percentage of DepDelays >30
@@ -19,7 +20,8 @@ system.time(
               substr('Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec ', (Month * 4) - 3, 3) AS Month,
               substr('Mon Tue Wed Thu Fri Sat Sun ', (DayOfWeek * 4) - 3, 3) AS Day,
               ROUND(CRSDepTime/100) AS Hour,
-              coalesce(round(count(case when DepDelay > 30 then 1 end)/(count(DepDelay)+.0)*100), 0) as PercentageDelay
+              coalesce(round(count(case when DepDelay > 30 then 1 end)/(count(DepDelay)+.0)*100), 0) as PercentageDelay,
+              count(DepDelay) AS count
               FROM airline1 
               GROUP BY Origin, Dest, Month, Day, Hour")
   )
@@ -33,7 +35,8 @@ system.time(
               substr('Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec ', (Month * 4) - 3, 3) AS Month,
               substr('Mon Tue Wed Thu Fri Sat Sun ', (DayOfWeek * 4) - 3, 3) AS Day,
               ROUND(CRSDepTime/100) AS Hour,
-              coalesce(round(count(case when DepDelay > 60 then 1 end)/(count(DepDelay)+.0)*100), 0) as PercentageDelay
+              coalesce(round(count(case when DepDelay > 60 then 1 end)/(count(DepDelay)+.0)*100), 0) as PercentageDelay,
+              count(DepDelay) AS count
               FROM airline1 
               GROUP BY Origin, Dest, Month, Day, Hour")
   )
@@ -47,7 +50,8 @@ system.time(
               substr('Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec ', (Month * 4) - 3, 3) AS Month,
               substr('Mon Tue Wed Thu Fri Sat Sun ', (DayOfWeek * 4) - 3, 3) AS Day,
               ROUND(CRSDepTime/100) AS Hour,
-              coalesce(round(count(case when DepDelay > 180 then 1 end)/(count(DepDelay)+.0)*100), 0) as PercentageDelay
+              coalesce(round(count(case when DepDelay > 180 then 1 end)/(count(DepDelay)+.0)*100), 0) as PercentageDelay,
+              count(DepDelay) AS count
               FROM airline1 
               GROUP BY Origin, Dest, Month, Day, Hour")
   )
