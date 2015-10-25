665f1f9689b7300d0cdbe3dff9606547f08f06a1
doutib <thibault.doutre@gmail.com>
2015-10-25 14:49:08 -0700

    ps6 2b

---
+++ ps6/ScriptQ2b.R
@@ -0,0 +1,59 @@
+
+# Load Database
+print('Loading db...')
+library(RSQLite)
+fileName="airline.db"
+drv = dbDriver("SQLite")
+db = dbConnect(drv, dbname = fileName)
+
+# Create a new table from subsetting airline
+print('Creating a subset of airline...')
+dbSendQuery(db, "CREATE TABLE airline1 AS 
+            SELECT *
+            FROM   airline
+            WHERE  DepDelay > -30
+            AND DepDelay < 720
+            AND DepDelay != 'NA'")
+
+# Create required table with percentage of DepDelays >30
+print('Aggregating DepDelay>30...')
+system.time(
+dbSendQuery(db,"CREATE TABLE Delay30 AS
+  SELECT 
+           Origin, 
+           Dest,
+           substr('Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec ', (Month * 4) - 3, 3) AS Month,
+           substr('Mon Tue Wed Thu Fri Sat Sun ', (DayOfWeek * 4) - 3, 3) AS Day,
+           ROUND(CRSDepTime/100) AS Hour,
+           coalesce(round(count(case when DepDelay > 30 then 1 end)/(count(DepDelay)+.0)*100), 0) as PercentageDelay
+           FROM airline1 
+           GROUP BY Origin, Dest, Month, Day, Hour")
+)
+# Create required table with percentage of DepDelays >60
+print('Aggregating DepDelay>60...')
+system.time(
+dbSendQuery(db,"CREATE TABLE Delay60 AS
+           SELECT 
+           Origin, 
+           Dest,
+           substr('Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec ', (Month * 4) - 3, 3) AS Month,
+           substr('Mon Tue Wed Thu Fri Sat Sun ', (DayOfWeek * 4) - 3, 3) AS Day,
+           ROUND(CRSDepTime/100) AS Hour,
+           coalesce(round(count(case when DepDelay > 60 then 1 end)/(count(DepDelay)+.0)*100), 0) as PercentageDelay
+           FROM airline1 
+           GROUP BY Origin, Dest, Month, Day, Hour")
+)
+# Create required table with percentage of DepDelays >180
+print('Aggregating DepDelay>180...')
+system.time(
+dbSendQuery(db,"CREATE TABLE Delay180 AS
+           SELECT 
+           Origin, 
+           Dest,
+           substr('Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec ', (Month * 4) - 3, 3) AS Month,
+           substr('Mon Tue Wed Thu Fri Sat Sun ', (DayOfWeek * 4) - 3, 3) AS Day,
+           ROUND(CRSDepTime/100) AS Hour,
+           coalesce(round(count(case when DepDelay > 180 then 1 end)/(count(DepDelay)+.0)*100), 0) as PercentageDelay
+           FROM airline1 
+           GROUP BY Origin, Dest, Month, Day, Hour")
+)
--- ps6/ps6.R
+++ ps6/ps6.R
@@ -1,5 +1,8 @@
 setwd('/Users/doutre/Documents/stat243/ps6')
 getwd()
+
+
+############################### 1 ###################################
 #Download
 #url = 'http://www.stat.berkeley.edu/share/paciorek/1987-2008.csvs.tgz'
 #download.file(url, .file)
@@ -46,17 +49,47 @@ dbSendQuery(conn = db,
             SecurityDelay           INTEGER,
             LateAircraftDelay       INTEGER)"
 )
-con=pipe(paste("bzcat", "1987.csv.bz2"), open = 'r')
+con=pipe(paste("bzcat", "./data/1987.csv.bz2"), open = 'r')
 lines = read.csv(con, header = TRUE)
 dbWriteTable(db, "y1987",lines)
 dbSendQuery(db,"INSERT INTO airline SELECT * FROM y1987")
 dbRemoveTable(db,"y1987")
 close(con)
-head(dbReadTable(db,"airline"))
 
+dbSendQuery(db, "CREATE TABLE airline1 AS 
+            SELECT *
+            FROM   airline
+            WHERE  DepDelay > -30
+            AND DepDelay < 720
+            AND DepDelay != 'NA'")
+
+dbListTables(db)
+head(dbGetQuery(db,"SELECT * FROM airline1"))
+
+
+system.time(
+dbSendQuery(db,"CREATE TABLE Delay30 AS
+  SELECT 
+        Origin, 
+        Dest,
+        substr('Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec ', (Month * 4) - 3, 3) AS Month,
+        substr('Mon Tue Wed Thu Fri Sat Sun ', (DayOfWeek * 4) - 3, 3) AS Day,
+        ROUND(CRSDepTime/100) AS Hour,
+        coalesce(round(count(case when DepDelay > 30 then 1 end)/(count(DepDelay)+.0)*100), 0) as PercentageDelay
+                FROM airline1 
+                GROUP BY Origin, Dest, Month, Day, Hour")
+)
+dbListTables(db)
+dbReadTable(db,"Delay30")
+dbRemoveTable(db,"Delay30")
+
+###### test #######
 
 # Write new table
-dbWriteTable(db, name = "y1987", value = '1987head.csv', row.names = FALSE, header = TRUE)
+dbWriteTable(db, name = "y1987", value = './data/1987head.csv', row.names = FALSE, header = TRUE)
+dbGetQuery(db, "SELECT COUNT(*) FROM y1987")
+dbReadTable(db,"y1987")
+
 
 # Get Fields
 dbListFields(db, "y1987")
@@ -80,7 +113,7 @@ nrow(dbGetQuery(db, "SELECT * FROM y1988"))
 nrow(dbGetQuery(db, "SELECT * FROM airline"))
 
 #delete table
-dbRemoveTable(db,"airline")
+dbRemoveTable(db,"y1987")
 
 #add index to database
 dbSendQuery(db,"CREATE UNIQUE INDEX id_y1987 ON y1987")
@@ -89,4 +122,15 @@ dbSendQuery(db,"ALTER TABLE y1988 ADD id")
 dbSendQuery(db,"CREATE UNIQUE INDEX id_y1988 ON y1987(id)")
 
 #
-file.size("./airline.db",units= "Gb")
+file.size("./airline.db")
+
+
+
+############################### 2 ###################################
+
+
+
+
+
+
+
