fe8e6e661c332848939a4f7682f43ca3f7e00654
doutib <thibault.doutre@gmail.com>
2015-10-31 17:08:38 -0700

    newQ3

--- ps6/ScriptQ3.R
+++ ps6/ScriptQ3.R
@@ -1,4 +1,3 @@
-
 # Load Database
 print('Loading db...')
 library(RSQLite)
@@ -6,6 +5,20 @@ fileName="airline.db"
 drv = dbDriver("SQLite")
 db = dbConnect(drv, dbname = fileName)
 
+# Create a new table from subsetting airline
+print('Creating a subset of airline...')
+dbSendQuery(db, "CREATE TABLE airline1 AS 
+            SELECT *
+            FROM   airline
+            WHERE  DepDelay > -30
+            AND DepDelay < 720
+            AND DepDelay != 'NA'")
+
+# Create index
+print('Creating Index...')
+dbSendQuery(db,"CREATE INDEX index_name ON airline1 (Origin,Dest,Month,DayOfWeek,CRSDepTime,DepDelay)")
+
+# Begin parallel programming
 library(doParallel)
 library(foreach)
 
@@ -13,29 +26,35 @@ nCores <- 4 # to set manually
 registerDoParallel(nCores) 
 
 system.time({
+  print('computing aggregation...')
   out=foreach(i = c(-1,180,60,30), .combine = c) %dopar% {
     if (i==-1){
       db0=dbConnect(drv, dbname = fileName)
       s=dbGetQuery(db0," SELECT 
                    Origin, 
                    Dest,
-                   substr('Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec ', (Month * 4) - 3, 3) AS Month,
-                   substr('Mon Tue Wed Thu Fri Sat Sun ', (DayOfWeek * 4) - 3, 3) AS Day,
+                   substr('Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec ',
+                   (Month * 4) - 3, 3) AS Month,
+                   substr('Mon Tue Wed Thu Fri Sat Sun ', (DayOfWeek * 4) - 
+                   3, 3) AS Day,
                    ROUND(CRSDepTime/100) AS Hour
                    FROM airline1 
                    GROUP BY Origin, Dest, Month, Day, Hour")
       return(s)
-      close(db0)
+      dbDisconnect(db0)
     }
     db1=dbConnect(drv, dbname = fileName)
     s=dbGetQuery(db1,paste("
                            SELECT 
-                           coalesce(round(count(case when DepDelay >",i," then 1 end)/(count(DepDelay)+.0)*100), 0) as PercentageDelay",i,"
+                           coalesce(round(count(case when DepDelay >",i,
+                           " then 1 end)/(count(DepDelay)+.0)*100), 0) as PercentageDelay",i,"
                            FROM airline1 
-                           GROUP BY Origin, Dest, Month, DayOfWeek, ROUND(CRSDepTime/100)",sep=""))
+                           GROUP BY Origin, Dest, Month, DayOfWeek, 
+                           ROUND(CRSDepTime/100)",sep=""))
     return(s)
-    close(db1)
+    dbDisconnect(db1)
   }
-  
+  print('writing table...')
   dbWriteTable(db,"summary",as.data.frame(out))
 })
+
